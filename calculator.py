693b0d98695423d0aaa12c73cff7242f36e81ba9
LostBee <savehoney188@gmail.com>
2025-08-09 19:13:56 +0200

    Letting user add input

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
-x = 2
-y = 2
-print(x+y)
+# Ask user for the 1st number and convert it into integer
+x = int(input("Enter the first number: "))
+# Ask user for the 2nd number and convert it into integer
+y = int(input("Enter the second number: "))
+# Calculate and print the sum
+print(x + y)
