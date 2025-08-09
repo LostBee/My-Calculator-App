46a6935cece14e057cd07a86f812a6e15b26d8d2
LostBee <savehoney188@gmail.com>
2025-08-09 19:55:32 +0200

    Fixing crashes with try...except

--- calculator.py
+++ calculator.py
@@ -1,18 +1,28 @@
 #Ask the user to choose an operation
 operation = input("What do you want to do? (add, subtract, multiply, divide): ")
 
-# Get the numbers
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
 
+# Let's make it better to stop it from crashing if input isn't an integer"
+# Get the numbers
+try:
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
+
+#This only runs if user enters something stupid
+except ValueError:
+    print("Error: You must enter valid numbers. Don't play with it")
+
+#If no errors, this runs as usual
 #Decide which operation to perform
-if operation == "add":
-    print(f"{x}+{y} = {x+y}")
-elif operation == "subtract":
-     print(f"{x}-{y} = {x-y}")
-elif operation == "multiply":
-      print(f"{x}*{y} = {x*y}")
-elif operation == "divide":
-      print(f"{x}/{y} = {x/y}")
-else:
-    print("Invalid Operation")
+
+else:   
+    if operation == "add":
+        print(f"{x}+{y} = {x+y}")
+    elif operation == "subtract":
+        print(f"{x}-{y} = {x-y}")
+    elif operation == "multiply":
+        print(f"{x}*{y} = {x*y}")
+    elif operation == "divide":
+        print(f"{x}/{y} = {x/y}")
+    else:
+        print("Invalid Operation")
